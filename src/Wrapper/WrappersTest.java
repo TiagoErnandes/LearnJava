7e750a973db877ea0b1d8d3d63fb215fa2c48992
Tiago <tiagoestevam7@hotmail.com>
2019-09-23 22:46:07 -0300

    Learn Java Add 23.09 Add "Init Wrappers"

---
+++ src/Wrapper/WrappersTest.java
@@ -0,0 +1,27 @@
+package Wrapper;
+
+public class WrappersTest {
+    public static void main(String[] args) {
+
+      byte n0;
+      short n1;
+      char n2;
+      int n3;
+      long n4;
+      float n5;
+      double n6 = 15;
+      boolean n7 = true;
+
+      Byte w0;
+      Short w1;
+      Character w2;
+      Integer w3;
+      Long w4;
+      Float w5;
+      Double w6;
+      Boolean w7;
+      w6 = 15D;
+      System.out.println(w6.toString());
+
+    }
+}
